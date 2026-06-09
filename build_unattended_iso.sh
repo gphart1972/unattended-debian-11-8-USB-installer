c7644df6f99a7cf8215cb244d3735910907e5f18
Greg Hart <BNFRIqC2q144@outlook.com>
2026-06-09 08:57:50 -0500

    Add script to build unattended Debian ISO
    This script automates the process of creating an unattended Debian ISO by mounting the original ISO, copying its contents, injecting preseed configurations, and rebuilding the ISO.

---
+++ build_unattended_iso.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+ISO_IN="debian-11.8.0-amd64-netinst.iso"
+ISO_OUT="debian-11.8.0-unattended.iso"
+WORKDIR="iso_work"
+MNTDIR="iso_mnt"
+
+# Clean old dirs
+sudo rm -rf "$WORKDIR" "$MNTDIR"
+mkdir -p "$WORKDIR" "$MNTDIR"
+
+echo ">>> Mounting original ISO..."
+sudo mount -o loop "$ISO_IN" "$MNTDIR"
+
+echo ">>> Copying ISO contents..."
+rsync -a "$MNTDIR/" "$WORKDIR/"
+
+echo ">>> Unmounting ISO..."
+sudo umount "$MNTDIR"
+
+echo ">>> Injecting preseed..."
+mkdir -p "$WORKDIR/preseed"
+cp preseed.cfg "$WORKDIR/preseed/custom.cfg"
+
+echo ">>> Patching isolinux/txt.cfg..."
+sed -i 's@append .*@append auto=true priority=critical vga=788 initrd=/install.amd/initrd.gz pr>
+    "$WORKDIR/isolinux/txt.cfg"
+
+echo ">>> Patching GRUB config..."
+sed -i 's@linux.*@linux /install.amd/vmlinuz auto=true priority=critical preseed/file=/cdrom/pr>
+    "$WORKDIR/boot/grub/grub.cfg"
+
+echo ">>> Rebuilding ISO..."
+xorriso -as mkisofs \
+  -o "$ISO_OUT" \
+  -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
+  -c isolinux/boot.cat \
+  -b isolinux/isolinux.bin \
+     -no-emul-boot -boot-load-size 4 -boot-info-table \
+  -eltorito-alt-boot \
+  -e boot/grub/efi.img \
+     -no-emul-boot \
+  "$WORKDIR"
+
+echo ">>> Done!"
+echo "Output ISO: $ISO_OUT"
